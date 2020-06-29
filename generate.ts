75833d32075d673a79de8d6c643383a7f1fdae49
Dmitriy Tatarintsev <tatarintsevd@gmail.com>
2020-06-29 18:09:02 +0400

    generate data 3

--- generate.ts
+++ generate.ts
@@ -11,10 +11,16 @@ for (let i = 0; i < points; i++) {
     const point: { [key: string]: number } = { point: i };
 
     for (let j = 0; j < colors; j++) {
-        point[String.fromCharCode(j + 65)] = i + j * 0.1 + Math.random();
+        point[String.fromCharCode(j + 65)] = 100 * Math.random();
     }
 
     data.push(point);
 }
 
+const a = [];
+for (let j = 0; j < colors; j++) {
+    a.push(String.fromCharCode(j + 65))
+}
+
+console.log(a.join(', '))
 writeJson("./data.json", data, { spaces: 2 });
